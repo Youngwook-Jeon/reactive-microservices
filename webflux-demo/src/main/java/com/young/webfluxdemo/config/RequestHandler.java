5ea3386340d32fa0dd03e6befbff58f466d33283
Young wook Jeon <ela87@naver.com>
2021-04-09 18:12:26 +0900

    Use router function to handle requests

---
+++ webflux-demo/src/main/java/com/young/webfluxdemo/config/RequestHandler.java
@@ -0,0 +1,38 @@
+package com.young.webfluxdemo.config;
+
+import com.young.webfluxdemo.dto.Response;
+import com.young.webfluxdemo.service.ReactiveMathService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class RequestHandler {
+
+    @Autowired
+    private ReactiveMathService mathService;
+
+    public Mono<ServerResponse> squareHandler(ServerRequest serverRequest) {
+        int input = Integer.parseInt(serverRequest.pathVariable("input"));
+        Mono<Response> responseMono = this.mathService.findSquare(input);
+        return ServerResponse.ok().body(responseMono, Response.class); // 퍼블리셔 타입으로 리턴하기
+    }
+
+    public Mono<ServerResponse> tableHandler(ServerRequest serverRequest) {
+        int input = Integer.parseInt(serverRequest.pathVariable("input"));
+        Flux<Response> responseFlux = this.mathService.multiplicationTable(input);
+        return ServerResponse.ok().body(responseFlux, Response.class); // 퍼블리셔 타입으로 리턴하기
+    }
+
+    public Mono<ServerResponse> tableStreamHandler(ServerRequest serverRequest) {
+        int input = Integer.parseInt(serverRequest.pathVariable("input"));
+        Flux<Response> responseFlux = this.mathService.multiplicationTable(input);
+        return ServerResponse.ok()
+                .contentType(MediaType.TEXT_EVENT_STREAM)
+                .body(responseFlux, Response.class); // 퍼블리셔 타입으로 리턴하기
+    }
+}
