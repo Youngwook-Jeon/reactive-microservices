bc1eef31ed7237e2c8b9e3e4070c7d99a5800cba
Young wook Jeon <ela87@naver.com>
2021-04-11 17:07:43 +0900

    Add functional endpoints

--- webflux-demo/src/main/java/com/young/webfluxdemo/config/RequestHandler.java
+++ webflux-demo/src/main/java/com/young/webfluxdemo/config/RequestHandler.java
@@ -1,6 +1,9 @@
 package com.young.webfluxdemo.config;
 
+import com.young.webfluxdemo.dto.InputFailedValidationResponse;
+import com.young.webfluxdemo.dto.MultiplyRequestDto;
 import com.young.webfluxdemo.dto.Response;
+import com.young.webfluxdemo.exception.InputValidationException;
 import com.young.webfluxdemo.service.ReactiveMathService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -34,5 +37,21 @@ public class RequestHandler {
         return ServerResponse.ok()
                 .contentType(MediaType.TEXT_EVENT_STREAM)
                 .body(responseFlux, Response.class); // 퍼블리셔 타입으로 리턴하기
+    }
+
+    public Mono<ServerResponse> multiplyHandler(ServerRequest serverRequest) {
+        Mono<MultiplyRequestDto> requestDtoMono = serverRequest.bodyToMono(MultiplyRequestDto.class);
+        Mono<Response> responseMono = this.mathService.multiply(requestDtoMono);
+        return ServerResponse.ok()
+                .body(responseMono, Response.class);
+    }
+
+    public Mono<ServerResponse> squareHandlerWithValidation(ServerRequest serverRequest) {
+        int input = Integer.parseInt(serverRequest.pathVariable("input"));
+        if (input < 10 || input > 20) {
+            return Mono.error(new InputValidationException(input));
+        }
+        Mono<Response> responseMono = this.mathService.findSquare(input);
+        return ServerResponse.ok().body(responseMono, Response.class); // 퍼블리셔 타입으로 리턴하기
     }
 }
--- webflux-demo/src/main/java/com/young/webfluxdemo/config/RouterConfig.java
+++ webflux-demo/src/main/java/com/young/webfluxdemo/config/RouterConfig.java
@@ -1,11 +1,14 @@
 package com.young.webfluxdemo.config;
 
+import com.young.webfluxdemo.dto.InputFailedValidationResponse;
+import com.young.webfluxdemo.exception.InputValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
-import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.reactive.function.server.*;
+import reactor.core.publisher.Mono;
+
+import java.util.function.BiFunction;
 
 @Configuration
 public class RouterConfig {
@@ -14,11 +17,37 @@ public class RouterConfig {
     private RequestHandler requestHandler;
 
     @Bean
-    public RouterFunction<ServerResponse> serverResponseRouterFunction() {
+    public RouterFunction<ServerResponse> highLevelRouter() {
         return RouterFunctions.route()
-                .GET("router/square/{input}", requestHandler::squareHandler)
-                .GET("router/table/{input}", requestHandler::tableHandler)
-                .GET("router/table/{input}/stream", requestHandler::tableStreamHandler)
+                .path("router", this::serverResponseRouterFunction)
                 .build();
+    }
+
+//    @Bean
+    private RouterFunction<ServerResponse> serverResponseRouterFunction() {
+        return RouterFunctions.route()
+                .GET(
+                        "/square/{input}",
+                        RequestPredicates.path("*/1?").or(RequestPredicates.path("*/20")),
+                        requestHandler::squareHandler
+                )
+                .GET("/square/{input}", req -> ServerResponse.badRequest().bodyValue("only 10-20 allowed"))
+                .GET("/table/{input}", requestHandler::tableHandler)
+                .GET("/table/{input}/stream", requestHandler::tableStreamHandler)
+                .POST("/multiply", requestHandler::multiplyHandler)
+                .GET("/square/{input}/validation", requestHandler::squareHandlerWithValidation)
+                .onError(InputValidationException.class, exceptionHandler())
+                .build();
+    }
+
+    private BiFunction<Throwable, ServerRequest, Mono<ServerResponse>> exceptionHandler() {
+        return (err, req) -> {
+            InputValidationException ex = (InputValidationException) err;
+            InputFailedValidationResponse response = new InputFailedValidationResponse();
+            response.setInput(ex.getInput());
+            response.setMessage(ex.getMessage());
+            response.setErrorCode(ex.getErrorCode());
+            return ServerResponse.badRequest().bodyValue(response);
+        };
     }
 }
